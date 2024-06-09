d13cf1a56e33612817cfae550d5b1aaa2b25fb17
nwleedev <nw.lee@outlook.com>
2024-06-09 22:40:34 +0900

    feat: footer

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-2 flex justify-center">
+      <div className="w-full max-w-screen-lg flex justify-center gap-x-4 text-sm">
+        <p className="border-b border-black">About</p>
+        <p className="border-b border-black">Terms</p>
+        <p className="border-b border-black">Privacy</p>
+      </div>
+    </footer>
+  );
+}
